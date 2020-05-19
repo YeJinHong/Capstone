f346f1d5abf1e1b5b8e7741fdcf61de5887704d6
altair0212 <62330009+altair0212@users.noreply.github.com>
2020-05-19 14:00:19 +0900

    map을 아스키값으로 변경

--- map/ko_braile_map.py
+++ map/ko_braile_map.py
@@ -2,153 +2,101 @@ from collections import defaultdict
 
 
 temp_chosung = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ","ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ","ㄲ","ㄸ","ㅃ","ㅆ","ㅉ"]
-temp_chosung_B = [chr(int('2808',16)),chr(int('2809',16)),chr(int('280A',16)),chr(int('2810',16)),chr(int('2811',16)),
-                  chr(int('2818',16)),chr(int('2820',16)),chr(int('281B',16)),chr(int('2828',16)),chr(int('2830',16)),
-                  chr(int('280B',16)),chr(int('2813',16)),chr(int('2819',16)),chr(int('281A',16)),
-                  "".join([chr(int('2820',16)),chr(int('2808',16))]),
-                  "".join([chr(int('2820',16)),chr(int('280A',16))]),
-                  "".join([chr(int('2820',16)),chr(int('2818',16))]),
-                  "".join([chr(int('2820',16)),chr(int('2820',16))]),
-                  "".join([chr(int('2820',16)),chr(int('2828',16))])]
+temp_chosung_B = ["`","C","I","\"","E",
+                  "^",",","G",".",";",
+                  "F","H","D","J",
+                  "".join([",","@"]),
+                  "".join([",","I"]),
+                  "".join([",","^"]),
+                  "".join([",",","]),
+                  "".join([",","."])
+                  ]
 
-temp_jungsung = ["ㅏ","ㅑ","ㅓ","ㅕ","ㅗ","ㅛ","ㅜ","ㅠ","ㅡ","ㅣ","ㅐ","ㅒ","ㅔ","ㅖ","ㅘ","ㅙ","ㅚ","ㅝ","ㅞ","ㅟ","ㅢ"]
-temp_jungsung_B = [chr(int('2823',16)),chr(int('281C',16)),chr(int('280E',16)),chr(int('2831',16)),chr(int('2825',16)),
-                   chr(int('282C',16)),chr(int('280D',16)),chr(int('2829',16)),chr(int('282A',16)),chr(int('2815',16)),chr(int('2817',16)),
-                   "".join([chr(int('281C',16)),chr(int('2817',16))]),
-                   chr(int('281D',16)),
-                   chr(int('280C',16)),
-                   chr(int('2827',16)),
-                   "".join([chr(int('2827',16)),chr(int('2817',16))]),
-                   chr(int('283D',16)),
-                   chr(int('280F',16)),
-                   "".join([chr(int('280F',16)),chr(int('2817',16))]),
-                   "".join([chr(int('280D',16)),chr(int('2817',16))]),
-                   chr(int('283A',16))]
 
-temp_jongsung = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ","ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ", "ㄳ","ㄵ","ㄶ","ㄺ","ㄻ","ㄼ","ㄽ","ㄾ","ㄿ","ㅀ","ㅄ","ㄲ","ㅆ"]
-temp_jongsung_B = [chr(int('2801',16)),
-                   chr(int('2812',16)),
-                   chr(int('2814',16)),
-                   chr(int('2802',16)),
-                   chr(int('2822',16)),
-                   chr(int('2803',16)),
-                   chr(int('2804',16)),
-                   chr(int('2836',16)),
-                   chr(int('2805',16)),
-                   chr(int('2806',16)),
-                   chr(int('2816',16)),
-                   chr(int('2826',16)),
-                   chr(int('2832',16)),
-                   chr(int('2834',16)),
-                   "".join([chr(int('2801',16)),chr(int('2804',16))]),
-                   "".join([chr(int('2812',16)),chr(int('2805',16))]),
-                   "".join([chr(int('2812',16)),chr(int('2834',16))]),
+temp_jungsung = ["ㅏ","ㅑ","ㅓ","ㅕ","ㅗ","ㅛ","ㅜ","ㅠ","ㅡ","ㅣ",
+                 "ㅐ","ㅒ","ㅔ","ㅖ","ㅘ","ㅙ","ㅚ","ㅝ","ㅞ","ㅟ","ㅢ"]
+temp_jungsung_B = ["<"," >"," S ",":"," U"," +"," M"," %", "["," O",
+                   "R","".join([">","R"]) ,"N","/","V", "".join(["V","R"]), "Y","P",
+                   "".join(["P","R"]),"".join(["M","R"]),"W"
 
-                   "".join([chr(int('2802',16)),chr(int('2801',16))]),
-                   "".join([chr(int('2802',16)),chr(int('2822',16))]),
-                   "".join([chr(int('2802',16)),chr(int('2803',16))]),
+                   ]
 
-                   "".join([chr(int('2802',16)),chr(int('2804',16))]),
-                   "".join([chr(int('2802',16)),chr(int('2826',16))]),
-                   "".join([chr(int('2802',16)),chr(int('2832',16))]),
-                   "".join([chr(int('2802',16)),chr(int('2834',16))]),
-                   "".join([chr(int('2803',16)),chr(int('2804',16))]),
-                   "".join([chr(int('2801',16)),chr(int('2801',16))]),
-                   chr(int('280C',16))]
+temp_jongsung = ["ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ","ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ",
+                 "ㄳ","ㄵ","ㄶ","ㄺ",
+                 "ㄻ","ㄼ","ㄽ","ㄾ",
+                 "ㄿ","ㅀ","ㅄ",
+                 "ㄲ","ㅆ"]
 
-temp_symbol = ["!","\"","#","$", "%","&","\'","(",")","*","+",",","-",".","/"]
-temp_symbol_B = [chr(int('282E',16)),
-                 chr(int('2810',16)),
-                 chr(int('283C',16)),
-                 chr(int('282B',16)),
+temp_jongsung_B = ["A","3","9","1",
+                   "5","B","\'","7",
+                   "K","2","6","8",
+                   "4","0",
+                   "".join(["A","\'"]), "".join(["3","K"]), "".join(["3","0"]),"".join(["1","A"]),
+                   "".join(["1","5"]),"".join(["1","B"]),"".join(["1","\'"]),"".join([" 1","8 "]),
+                   "".join(["1","4"]),"".join(["1","0"]),"".join(["B","\'"]),
+                   "".join(["A","A"]),"/"
+                   ]
 
-                 chr(int('2829',16)),
-                 chr(int('282F',16)),
-                 chr(int('2804',16)),
+temp_symbol = ["!","\"","#","$",
+               "%","&","\'","(",
+               ")","*","+",",","-",".","/"]
+temp_symbol_B = ["6","8","#","$",
+                "".join(["0","p"]),"&","".join([",","8"]),"".join(["8","\'"]),
+                "".join([",","0"]),"*","5","\"","_","4","".join(["_","/"])
+                ]
+#여는 큰 따옴표는 8 / 닫는 큰 따옴표는 0
+#물음표 아직 안 넣어서 오류 날지도 모름.
+temp_number = [0,1,2,3,4,5,6,7,8,9]
+temp_number_B = ["".join(["#","J"]),
+                 "".join(["#","A"]),
+                 "".join(["#","B"]),
+                 "".join(["#","C"]),
+                 "".join(["#","D"]),
+                 "".join(["#","E"]),
+                 "".join(["#","F"]),
+                 "".join(["#","G"]),
+                 "".join(["#","H"]),
+                 "".join(["#","I"]),
 
-                 chr(int('2837',16)),
-                 chr(int('283E',16)),
-                 chr(int('2821',16)),
-                 chr(int('282C',16)),
-
-                 chr(int('2820',16)),
-                 chr(int('2824',16)),
-                 chr(int('2828',16)),
-                 chr(int('280C',16))
 
                  ]
 
-
-temp_number = [0,1,2,3,4,5,6,7,8,9]
-temp_number_B = [chr(int('281A',16)),chr(int('2801',16)),chr(int('2803',16)),chr(int('2809',16)),chr(int('2819',16)),chr(int('2811',16)),
-                 chr(int('280B',16)),chr(int('281B',16)),chr(int('2813',16)),chr(int('280A',16))]
-
 temp_UpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-temp_UpperCase_B=["".join([chr(int('2820', 16)),chr(int('2801', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2803', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2809', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2819', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2811', 16))]),
+temp_UpperCase_B=["".join([",","A"]),
+                  "".join([ ",","B" ]),
+                  "".join([",","C"]),
+                  "".join([",","D" ]),
+                  "".join([",","E "]),
 
-                  "".join([chr(int('2820', 16)), chr(int('280B', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('281B', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2813', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('280A', 16))]),
+                  "".join([" ,","F "]),
+                  "".join([",","G "]),
+                  "".join([",","H "]),
+                  "".join([",","I"]),
 
-                  "".join([chr(int('2820', 16)), chr(int('281A', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2805', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2807', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('280D', 16))]),
+                  "".join([",","J"]),
+                  "".join([",","K "]),
+                  "".join([",","L "]),
+                  "".join([",","M "]),
 
-                  "".join([chr(int('2820', 16)), chr(int('281D', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2815', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('280F', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('281F', 16))]),
+                  "".join([",", "N "]),
+                  "".join([",","O"]),
+                  "".join([",", "P "]),
+                  "".join([",","Q"]),
 
-                  "".join([chr(int('2820', 16)), chr(int('2817', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('280E', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('281E', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2825', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2827', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('283A', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('282D', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('283D', 16))]),
-                  "".join([chr(int('2820', 16)), chr(int('2835', 16))])
+                  "".join([",","R"]),
+                  "".join([",", "S "]),
+                  "".join([",","T"]),
+                  "".join([",","U "]),
+                  "".join([",","V"]),
+                  "".join([",", "W "]),
+                  "".join([",","X"]),
+                  "".join([",", "Y "]),
+                  "".join([",","Z"]),
 
                   ]
 
 temp_LowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-temp_LowerCase_B=[chr(int('2801', 16)),
-                  chr(int('2803', 16)),
-                  chr(int('2809', 16)),
-                  chr(int('2819', 16)),
-                  chr(int('2811', 16)),
-
-                  chr(int('280B', 16)),
-                  chr(int('281B', 16)),
-                  chr(int('2813', 16)),
-                  chr(int('280A', 16)),
-
-                  chr(int('281A', 16)),
-                  chr(int('2805', 16)),
-                  chr(int('2807', 16)),
-                  chr(int('280D', 16)),
-
-                  chr(int('281D', 16)),
-                  chr(int('2815', 16)),
-                  chr(int('280F', 16)),
-                  chr(int('281F', 16)),
-
-                  chr(int('2817', 16)),
-                  chr(int('280E', 16)),
-                  chr(int('281E', 16)),
-                  chr(int('2825', 16)),
-                  chr(int('2827', 16)),
-                  chr(int('283A', 16)),
-                  chr(int('282D', 16)),
-                  chr(int('283D', 16)),
-                  chr(int('2835', 16))
-                  ]
+temp_LowerCase_B=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
 
 #construct mapping_dict
 map_chosung = defaultdict()
@@ -166,6 +114,7 @@ map_Symbol = defaultdict()
 for idx, sung in enumerate(temp_jongsung):
     map_jongsung[sung]=temp_jongsung_B[idx]
 map_jongsung[" "]=""
+
 
 for idx, sung in enumerate(temp_jungsung):
     map_jungsung[sung]=temp_jungsung_B[idx]
