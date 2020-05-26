a174c0650c85565dde2bac2693616a00bc70c04d
gpdnjs421 <gpdnjs421@gmail.com>
2020-05-26 21:41:15 +0900

    어절 단위로 약어 처리, #은 숫자 앞자리에만 붙여지게, 숫자 자릿수 표시하는 ,의 표기 다르게 수정

--- map/ko_braile_map.py
+++ map/ko_braile_map.py
@@ -47,16 +47,16 @@ temp_symbol_B = ["6","8","#","$",
 #여는 큰 따옴표는 8 / 닫는 큰 따옴표는 0
 #물음표 아직 안 넣어서 오류 날지도 모름.
 temp_number = [0,1,2,3,4,5,6,7,8,9]
-temp_number_B = ["".join(["#","J"]),
-                 "".join(["#","A"]),
-                 "".join(["#","B"]),
-                 "".join(["#","C"]),
-                 "".join(["#","D"]),
-                 "".join(["#","E"]),
-                 "".join(["#","F"]),
-                 "".join(["#","G"]),
-                 "".join(["#","H"]),
-                 "".join(["#","I"]),
+temp_number_B = ["".join(["","J"]),
+                 "".join(["","A"]),
+                 "".join(["","B"]),
+                 "".join(["","C"]),
+                 "".join(["","D"]),
+                 "".join(["","E"]),
+                 "".join(["","F"]),
+                 "".join(["","G"]),
+                 "".join(["","H"]),
+                 "".join(["","I"]),
                  ]
 
 temp_UpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -96,6 +96,10 @@ temp_UpperCase_B=["".join([",","A"]),
 temp_LowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 temp_LowerCase_B=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
 
+#앞음절-약어 사용, 뒷음절-약어 미사용
+temp_abbreviation_word = ["그래서", "그러나", "그러면", "그러므로", "그런데", "그리고", "그리하여"]
+temp_abbreviation_word_B = ["as", "ac", "a3", "a5", "an", "au", "a:"]
+
 #construct mapping_dict
 map_chosung = defaultdict()
 map_jungsung = defaultdict()
@@ -106,6 +110,8 @@ map_UpperCase = defaultdict()
 map_LowerCase = defaultdict()
 
 map_Symbol = defaultdict()
+
+map_abbreviation_word = defaultdict()
 
 
 
@@ -132,3 +138,5 @@ for idx, low in enumerate(temp_LowerCase):
 for idx, sym in enumerate(temp_symbol):
     map_Symbol[sym] = temp_symbol_B[idx]
 
+for idx, abb in enumerate(temp_abbreviation_word):
+    map_abbreviation_word[abb] = temp_abbreviation_word_B[idx]
